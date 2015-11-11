8de7a02c6646d8ffd31b1938dcc131893172904a
devIceMan <gizatulin.timur@gmail.com>
2015-11-11 20:04:57 +0300

    * update

--- test.js
+++ test.js
@@ -1,7 +1,278 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+var data = [
+  {
+    "Display": "Общая длина ограждения, пог.м.",
+    "Name": "TotalLengthOfFence"
+  },
+  {
+    "Display": "Высота ограждения, м",
+    "Name": "HeightOfFence"
+  },
+  {
+    "Display": "Состояние подвала",
+    "Name": "BasementStatus"
+  },
+  {
+    "Display": "Высота следов затопления подвала, м",
+    "Name": "HeightFloodingTracesOfBasement"
+  },
+  {
+    "Display": "Состояние предполагаемого места установки модуля ГВС",
+    "Name": "StatusAllegedLocationOfModule"
+  },
+  {
+    "Display": "Высота следов затопления в предполагаемом месте установки модуля ГВС, м",
+    "Name": "HeightFloodingTracesOfAllegedLocation"
+  },
+  {
+    "Display": "Пол в месте предполагаемой установки модуля ГВС",
+    "Name": "FloorInAllegedLocationOfModule"
+  },
+  {
+    "Display": "Ширина входа в подвал, мм",
+    "Name": "EntranceWidthOfBasement"
+  },
+  {
+    "Display": "Высота входа в подвал, мм",
+    "Name": "EntranceHeightOfBasement"
+  },
+  {
+    "Display": "Ширина самого маленького проема на пути следования от входа к месту предполагаемой установки, мм",
+    "Name": "WidthSmallestOpening"
+  },
+  {
+    "Display": "Высота самого маленького проема на пути следования от входа к месту предполагаемой установки, мм",
+    "Name": "HeightSmallestOpening"
+  },
+  {
+    "Display": "Ширина пустого места свободного от трубопроводов и коммуникаций в месте предполагаемой установки модуля ГВС, м",
+    "Name": "WidthOfEmptySpace"
+  },
+  {
+    "Display": "Длина пустого места свободного от трубопроводов и коммуникаций в месте предполагаемой установки модуля ГВС, м",
+    "Name": "LengthOfEmptySpace"
+  },
+  {
+    "Display": "Высота пустого места свободного от трубопроводов и коммуникаций в месте предполагаемой установки модуля ГВС, м",
+    "Name": "HeightOfEmptySpace"
+  },
+  {
+    "Display": "Наличие освещения в подвале",
+    "Name": "LightPresenceInBasement"
+  },
+  {
+    "Display": "Наличие освещения в месте установки модуля ГВС",
+    "Name": "LightPresenceInAllegedLocationOfModule"
+  },
+  {
+    "Display": "Необходимость установки антивандального ограждения",
+    "Name": "NeedInstalAntiVandalFence"
+  },
+  {
+    "Display": "Схема модуля ГВС",
+    "Name": "SchemeOfModuleGVS"
+  },
+  {
+    "Display": "Стоимость материалов и работ по возведению фундамента (удалить)",
+    "Name": "CostOfFoundation"
+  },
+  {
+    "Display": "Стоимость материалов и работ по возведению антивандального ограждения (удалить)",
+    "Name": "CostOfAntiVandalFence"
+  },
+  {
+    "Display": "Стоимость материалов и работ по прокладке линии эл.питания (удалить)",
+    "Name": "CostOfLayingLEP"
+  },
+  {
+    "Display": "Стоимость материалов и работ по установке дренажной системы (удалить)",
+    "Name": "CostOfDrainageSystem"
+  },
+  {
+    "Display": "Стоимость материалов и работ по диспетчеризации (удалить)",
+    "Name": "CostOfDispatch"
+  },
+  {
+    "Display": "ПНР (удалить)",
+    "Name": "PNR"
+  },
+  {
+    "Display": "ПНР по узлу регулирования системы отопления (удалить)",
+    "Name": "PNRNodeSystem"
+  },
+  {
+    "Display": "Проектные работы по модулю ГВС (удалить)",
+    "Name": "ModuleProjectWork"
+  },
+  {
+    "Display": "Проектные работы по узлу регулирования системы отопления (удалить)",
+    "Name": "NodeSystemProjectWork"
+  },
+  {
+    "Display": "Балансировка внутренней системы ГВС (удалить)",
+    "Name": "BalanceInnerSystemGVS"
+  },
+  {
+    "Display": "Стоимость материалов и работ по установке насоса повышения давления холодной воды (удалить)",
+    "Name": "CostPumpColdWater"
+  },
+  {
+    "Display": "Стоимость оборудования по отечественной цене для ГВС (бывш. Итоговая отечественная стоимость)",
+    "Name": "TotalDomesticCost"
+  },
+  {
+    "Display": "Стоимость оборудования по импортной цене для ГВС (бывш. Итоговая импортная стоимость)",
+    "Name": "TotalImportCost"
+  },
+  {
+    "Display": "Стоимость оборудования по внутренней цене для ГВС (бывш. Итоговая внутренняя стоимость)",
+    "Name": "TotalInnerCost"
+  },
+  {
+    "Display": "Стоимость оборудования по отечественной цене для отопления (бывш. Стоимость модуля с применением отечественного оборудования)",
+    "Name": "CostUsingDomesticEquipment"
+  },
+  {
+    "Display": "Стоимость оборудования по импортной цене для отопления (бывш. Стоимость модуля с применением импортного оборудования)",
+    "Name": "CostUsingImportEquipment"
+  },
+  {
+    "Display": "Стоимость оборудования по внутренней цене для отопления (бывш. Стоимость модуля по внутренней цене)",
+    "Name": "CostUsingInternalPrice"
+  },
+  {
+    "Display": "Место для установки модуля",
+    "Name": "PlaceForOnstallationOfModuleScroll"
+  },
+  {
+    "Display": "Материал перекрытий места установки",
+    "Name": "CeilingsMaterialOfInstallationPlace"
+  },
+  {
+    "Display": "Материал стен места установки",
+    "Name": "WallMaterialOfInstallationPlace"
+  },
+  {
+    "Display": "Доп. описание",
+    "Name": "Description"
+  },
+  {
+    "Display": "Стоимость материалов и работ по прокладке трубопроводов",
+    "Name": "CostOfLayingPipeline"
+  },
+  {
+    "Display": "Наименование итоговой стоимости",
+    "Name": "FinalCostName"
+  },
+  {
+    "Display": "Стоимость материалов и работ по возведению фундамента",
+    "Name": "CostOfFoundationNew"
+  },
+  {
+    "Display": "Стоимость материалов и работ по возведению антивандального ограждения",
+    "Name": "CostOfAntiVandalFenceNew"
+  },
+  {
+    "Display": "Стоимость материалов и работ по прокладке линии эл.питания",
+    "Name": "CostOfLayingLEPNew"
+  },
+  {
+    "Display": "Стоимость материалов и работ по установке дренажной системы",
+    "Name": "CostOfDrainageSystemNew"
+  },
+  {
+    "Display": "Стоимость материалов и работ по диспетчеризации",
+    "Name": "CostOfDispatchNew"
+  },
+  {
+    "Display": "ПНР по модулю ГВС",
+    "Name": "PNRNew"
+  },
+  {
+    "Display": "ПНР по узлу регулирования системы отопления",
+    "Name": "PNRNodeSystemNew"
+  },
+  {
+    "Display": "Проектные работы по модулю ГВС",
+    "Name": "ModuleProjectWorkNew"
+  },
+  {
+    "Display": "Проектные работы по узлу регулирования системы отопления",
+    "Name": "NodeSystemProjectWorkNew"
+  },
+  {
+    "Display": "Балансировка внутренней системы ГВС",
+    "Name": "BalanceInnerSystemGVSNew"
+  },
+  {
+    "Display": "Стоимость материалов и работ по установке насоса повышения давления холодной воды",
+    "Name": "CostPumpColdWaterNew"
+  },
+  {
+    "Display": "Количество модулей, шт",
+    "Name": "ModulesNumber"
+  },
+  {
+    "Display": "GUID",
+    "Name": "GUID"
+  },
+  {
+    "Display": "Reference",
+    "Name": "Reference"
+  },
+  {
+    "Display": "Created",
+    "Name": "Created"
+  },
+  {
+    "Display": "Updated",
+    "Name": "Updated"
+  },
+  {
+    "Display": "Идентификатор",
+    "Name": "Id"
+  }
+];
+
+
+var strings = data.map(function (x) { return x.Display });
+var map = strings.map(function (x) { return { phrase: x, parts: x.split(' ') } });
+
+var items = [],
+  visited = {};
+
+function visit(minLength, x) {
+  if (visited[x.Name]) return;
+  minLength = minLength || 1;
+  var parts = x.Display.split(' '),
+    idx = minLength > parts.length ? parts.length : minLength,
+    weight = [];
+
+  while (idx >= minLength) {
+    var phrase = parts.slice(0, idx).join(' ');
+
+    var same = data.filter(function (x) {
+      return x.Display.indexOf(phrase) == 0;
+    });
+
+    weight.push([phrase, same]);
+    idx--;
+  }
+
+  if (weight.length == 0) {
+    items.push([x.Display, x]);
+  }
+  else {
+    var maxWeight = weight.reduce(function (a, b, i, ar) {
+      return a[1].length < b[1].length ? b : a
+    });
+    items.push(maxWeight);
+  }
+
+  weight = [];
+
+  visited[x.Name] = true;
 }
 
-var timer = setInterval(function(){
+data.forEach(visit.bind(this,3));
 
-}, 1000);
+console.log(items);
